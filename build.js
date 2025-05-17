2830b28fe997661234307fbda01fc45dd4b27a91
cariotan <a@cario.me>
2025-05-17 19:46:00 +1000

    Creates public folder if it does not exist.

--- build.js
+++ build.js
@@ -6,4 +6,9 @@ const index = fs.readFileSync('views/index.ejs', 'utf-8')
 
 const indexHtml = ejs.render(base, { body: index })
 
+// Ensure the 'public' folder exists
+if (!fs.existsSync('public')) {
+  fs.mkdirSync('public', { recursive: true })
+}
+
 fs.writeFileSync('public/index.html', indexHtml)
